af5d3b641cebb836afe4e1d3fc10d07378c120a6
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2025-11-06 21:27:25 +0300

    inital setup filament setup admin dashboard

---
+++ app/Filament/Resources/Brands/Schemas/BrandForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Brands\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Str;
+class BrandForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+               TextInput::make('name')
+                        ->required()
+                        ->reactive()
+                        ->afterStateUpdated(function ($state, callable $set) {
+                            $set('slug', Str::slug($state));
+                        }),
+                TextInput::make('slug')
+                    ->required(),
+                FileUpload::make('image')
+                    ->image(),
+                Toggle::make('is_active')
+                    ->required(),
+            ]);
+    }
+}
