2202294c8922d19596597ceb476394fc835eb806
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2025-12-22 09:56:33 +0300

    HomePage Modification - use of Livewire wire:navigation to switch between pages faster just like an spa

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -1,14 +1,22 @@
 <?php
 
 namespace App\Livewire;
-
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Brand;
+
+#[Title('Home Page - My E-commerce Site')]
 
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', true)->get();
+        return view('livewire.home-page',[
+            'brands' => $brands
+        ]);
     }
 }
-#according to the tutorial i am following this is like a controller 
+#according to the tutorial i am following this is like a controller
+#its for the business logic of the home page
