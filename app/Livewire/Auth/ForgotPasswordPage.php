0daba9587f64768b71654c419086dd37e9dc9381
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2025-12-30 15:30:16 +0300

    Forgot Password and Reset Password Feature Completed

--- app/Livewire/Auth/ForgotPasswordPage.php
+++ app/Livewire/Auth/ForgotPasswordPage.php
@@ -2,10 +2,35 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Password;
 use Livewire\Component;
+use Livewire\Attributes\Title;
+#[Title('Forgot Password')]
 
 class ForgotPasswordPage extends Component
 {
+    public $email;
+
+    public function save()
+    {
+        // Validate the email input
+        $this->validate([
+            'email' => 'required|email|exists:users,email|max:255',
+        ]);
+
+        $status = Password::sendResetLink(
+            ['email' => $this->email]
+        );
+
+        if ($status === Password::RESET_LINK_SENT) {
+            session()->flash('success', 'Password reset link sent to your email.');
+            $this->email = '';
+        } else {
+            session()->flash('error', 'Unable to send password reset link. Please try again later.');
+        }
+
+
+    }
     public function render()
     {
         return view('livewire.auth.forgot-password-page');
--- app/Livewire/Auth/ResetPasswordPage.php
+++ app/Livewire/Auth/ResetPasswordPage.php
@@ -3,9 +3,62 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use App\Models\User;
 
+#[Title('Reset Password')]
 class ResetPasswordPage extends Component
 {
+
+    public $token;
+
+    #[Url]
+    public $email;
+    public $password;
+    public $password_confirmation;
+
+    public function mount($token)
+    {
+        $this->token = $token;
+    }
+
+    public function save()
+    {
+        $this->validate([
+            'token' => 'required',
+            'email' => 'required|email|exists:users,email|max:255',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            [
+                'email' => $this->email,
+                'password' => $this->password,
+                'password_confirmation' => $this->password_confirmation,
+                'token' => $this->token,
+            ],
+            function (User $user, string $password) {
+                $password = $this->password;
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                    //'remember_token' => Str::random(60),
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+                    ? redirect('/login'):session()->flash('error', 'Failed to reset password. Please try again.');
+    }
+
+
     public function render()
     {
         return view('livewire.auth.reset-password-page');
