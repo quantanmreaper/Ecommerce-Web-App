90ba70cb9e3a8c58672d2e59b5b12ad95172b8d6
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2026-01-02 21:44:49 +0300

    Checkout Feature Implemented Stripe API payment successfull

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,8 +3,13 @@
 namespace App\Livewire;
 
 use App\Helpers\CartManagement;
+use App\Models\Order;
+use App\Models\Address;
+use Illuminate\Contracts\Auth\Guard;
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
 
 #[Title ('Checkout Page')]
 class CheckoutPage extends Component
@@ -17,6 +22,12 @@ class CheckoutPage extends Component
     public $state;
     public $zip_code;
     public $payment_method;
+    public function mount(){
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        if(empty($cart_items)){
+            return redirect()->to('/products');
+        }
+    }
 
     public function placeOrder(){
         $this->validate([
@@ -27,8 +38,68 @@ class CheckoutPage extends Component
             'city' => 'required|string|max:255',
             'state' => 'required|string|max:255',
             'zip_code' => 'required|string|max:20',
-            'payment_method' => 'required|in:cod,stripe',
+            'payment_method' => 'required',
         ]);
+
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $line_items = [];
+        foreach($cart_items as $item){
+            $line_items[] = [
+
+                'price_data' => [
+                    'currency'=> 'usd',
+                    'unit_amount'=> $item['unit_amount'] * 100,
+                    'product_data'=> [
+                        'name'=> $item['product_name'],
+                    ],
+                ],
+                'quantity' => $item['quantity'],
+            ];
+        }
+
+        $order = new Order();
+        $order->user_id = auth()->guard()->user()->id;
+        $order->total_amount = CartManagement::calculateGrandTotal($cart_items);
+        $order->payment_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'new';
+        $order->currency = 'KES';
+        $order->shipping_amount = 0;
+        $order->shipping_method = 'none';
+        $order->notes = 'Order Placed by'. auth()->guard()->user()->name;
+
+        $address = new Address();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->street_address = $this->street_address;
+        $address->city = $this->city;
+        $address->state = $this->state;
+        $address->zip_code = $this->zip_code;
+
+        $redirect_url = '';
+
+        if($this->payment_method == 'stripe'){
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+            $sessionCheckout = Session::create([
+                'payment_method_types'=> [ 'card' ],
+                'customer_email'=> auth()->guard()->user()->email,
+                'line_items'=> $line_items,
+                'mode'=> 'payment',
+                'success_url'=> route('success') . '?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url'=> route('cancel'),
+            ]);
+            $redirect_url = $sessionCheckout->url;
+        }else{
+                $redirect_url = route('success');
+        }
+
+        $order->save();
+        $address->order_id = $order->id;
+        $address->save();
+        $order->items()->createMany($cart_items);
+        CartManagement::clearCartItems();
+        return redirect($redirect_url);
     }
 
     public function render()
