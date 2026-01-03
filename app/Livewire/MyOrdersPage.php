f874a11e5f17b6a7348f70883ffc4469b4a65f2d
quantanmreaper <138207080+quantanmreaper@users.noreply.github.com>
2026-01-03 11:40:35 +0300

    User Orders Page

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Order;
 
+#[Title('My Orders Page')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.my-orders-page');
+        $my_orders = Order::where('user_id', auth()->guard()->user()->id)->with('address')->latest()->paginate(5);
+        return view('livewire.my-orders-page', [
+            'my_orders' => $my_orders,
+        ]);
     }
 }
